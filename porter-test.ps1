28f70b788ab07d1de24aba90a89a67ff7aa5f251
James Dawson <james@readsource.co.uk>
2019-09-16 14:33:47 +0100

    Initial commit of an example ansible playbook packaged as a CNAB

---
+++ porter-test.ps1
@@ -0,0 +1,34 @@
+param
+(
+    [parameter(mandatory=$true)]
+    [string] $environmentName,
+
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({Test-Path $_})]
+    [string] $credentialFile = "./porter-dev-creds.yml",
+
+    [ValidateSet("install","upgrade","uninstall", ignorecase=$true)]
+    [string] $action = "install",
+
+    [switch] $skipBuild
+)
+
+$ErrorActionPreference = 'Stop'
+$here = Split-Path -Parent $PSCommandPath
+
+if ( !(Test-Path env:AZURE_SECRET) )
+{
+    Write-Error "You must preload your shell with the required environment variables for Azure SPN authentication:`n`tAZURE_CLIENT_ID`n`tAZURE_SECRET`n`tAZURE_SUBSCRIPTION_ID`n`tAZURE_TENANT"
+}
+
+Push-Location $here
+if (!$skipBuild)
+{
+    # ensure the base image is built
+    docker build -t cnab-ansible-base -f Dockerfile.base .
+    # build the bundle
+    porter build
+}
+
+porter $action --param environment_name=$environmentName --cred $credentialFile
+Pop-Location
